ae92a9d55e78ce2f304507eb08215a81f4390859
Chris Liu <acciochris@outlook.com>
2024-01-03 14:45:40 -0800

    Add db parallel creation

--- lib/db.py
+++ lib/db.py
@@ -1,11 +1,25 @@
-from sqlalchemy import create_engine, String, ForeignKey
+from os import PathLike
+from functools import partial
+from pathlib import Path
+from concurrent.futures import ProcessPoolExecutor, as_completed
+
+import cv2 as cv
+from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import (
     DeclarativeBase,
     MappedAsDataclass,
     Mapped,
     mapped_column,
     relationship,
+    sessionmaker,
 )
+
+from component import (
+    generate_categories,
+    get_components,
+    classify_components,
+)
+from utils import numpy_to_bytes, bytes_to_numpy
 
 
 class Base(MappedAsDataclass, DeclarativeBase):
@@ -33,7 +47,7 @@ class ComponentCategoryVariant(Base):
     category_id: Mapped[int] = mapped_column(
         ForeignKey("component_category.id"), init=False
     )
-    content: Mapped[bytes]
+    content: Mapped[bytes | None] = mapped_column(default=None)
 
     category: Mapped["ComponentCategory"] = relationship(
         back_populates="variants", default=None
@@ -48,11 +62,11 @@ class Component(Base):
     category_id: Mapped[int] = mapped_column(
         ForeignKey("component_category.id"), init=False
     )
-    content: Mapped[bytes]
     x: Mapped[int]
     y: Mapped[int]
     w: Mapped[int]
     h: Mapped[int]
+    content: Mapped[bytes | None] = mapped_column(default=None)
 
     category: Mapped["ComponentCategory"] = relationship(
         back_populates="components", default=None
@@ -64,10 +78,96 @@ class Page(Base):
     __tablename__ = "page"
 
     id: Mapped[int] = mapped_column(primary_key=True, init=False)
-    content: Mapped[bytes]
     w: Mapped[int]
     h: Mapped[int]
+    name: Mapped[str | None] = mapped_column(default=None)
+    content: Mapped[bytes | None] = mapped_column(default=None)
 
     components: Mapped[list["Component"]] = relationship(
         back_populates="page", default_factory=list
     )
+
+
+def create_tables(engine):
+    Base.metadata.create_all(engine)
+
+
+def _classify(targets, img_info):
+    path, img = img_info
+    print(path)
+    coords, components = get_components(img)
+
+    results = classify_components(components, targets)
+    return path, img, results, coords, components
+
+
+def _images(pages_path: str | PathLike):
+    for path in Path(pages_path).iterdir():
+        img = cv.imread(str(path))
+        yield path, img
+
+
+def create_db(
+    engine,
+    category_path: str | PathLike,
+    pages_path: str | PathLike,
+    store_content: bool = False,
+    workers: int = 1,
+):
+    create_tables(engine)
+    Session = sessionmaker(bind=engine)
+
+    # add categories and variants
+    paths, contents = generate_categories(category_path)
+    segments = [path.stem.split(".") for path in paths]
+    category_names = {s[0] for s in segments}
+    categories = {name: ComponentCategory(name) for name in category_names}
+    variants = []
+
+    for s, c in zip(segments, contents):
+        variant = ComponentCategoryVariant(content=numpy_to_bytes(c))
+        categories[s[0]].variants.append(variant)
+        variants.append(variant)
+
+    with Session() as session:
+        session.expire_on_commit = False
+        session.add_all(categories.values())
+        session.commit()
+
+    # add pages and components
+    targets = [bytes_to_numpy(v.content) for v in variants]
+
+    _classify_func = partial(_classify, targets)
+    with ProcessPoolExecutor(max_workers=workers) as executor:
+        futures = [
+            executor.submit(_classify_func, img_info)
+            for img_info in _images(pages_path)
+        ]
+        for future in as_completed(futures):
+            path, img, results, coords, components = future.result()
+
+            with Session(expire_on_commit=False) as session:
+                page = Page(
+                    w=img.shape[1],
+                    h=img.shape[0],
+                    name=path.name,
+                    content=numpy_to_bytes(img) if store_content else None,
+                )
+
+                for (i, row), content in zip(coords.iterrows(), components):
+                    if row.w * row.h > 40000:
+                        continue  # too big
+
+                    component = Component(
+                        x=int(row.x),
+                        y=int(row.y),
+                        w=int(row.w),
+                        h=int(row.h),
+                        content=numpy_to_bytes(content) if store_content else None,
+                    )
+                    component.category = categories[variants[results[i]].category.name]
+                    component.page = page
+                    session.add(component)
+
+                session.add(page)
+                session.commit()
